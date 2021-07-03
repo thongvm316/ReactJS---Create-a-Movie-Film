d334006570385be832242544f52a4f3906daeb5b
BM James <thongvm@brickmate.kr>
2021-07-03 12:02:39 +0700

    config for deploy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   API_URL,
   API_KEY,
   IMAGE_BASE_URL,
   BACKDROP_SIZE,
   POSTER_SIZE,
-} from '../../config';
-import HeroImage from '../elements/HeroImage/HeroImage';
-import SearchBar from '../elements/SearchBar/SearchBar';
-import FourColGrid from '../elements/FourColGrid/FourColGrid';
-import MovieThumb from '../elements/MovieThumb/MovieThumb';
-import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
-import Spinner from '../elements/Spinner/Spinner';
-import './Home.css';
+} from '../../config'
+import HeroImage from '../elements/HeroImage/HeroImage'
+import SearchBar from '../elements/SearchBar/SearchBar'
+import FourColGrid from '../elements/FourColGrid/FourColGrid'
+import MovieThumb from '../elements/MovieThumb/MovieThumb'
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn'
+import Spinner from '../elements/Spinner/Spinner'
+import './Home.css'
 
 class Home extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       movies: [],
       heroImage: null,
@@ -24,70 +24,68 @@ class Home extends Component {
       currentPage: 0,
       totalPages: 0,
       searchTerm: '',
-    };
+    }
   }
 
   componentDidMount() {
-    this.setState({ loading: true });
-    const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=3`;
+    this.setState({ loading: true })
+    const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=3`
     // console.log(endPoint);
-    this.fetchItems(endPoint);
+    this.fetchItems(endPoint)
   }
 
   searchIterms = (searchTerm) => {
-    console.log(searchTerm);
-    let endPoint = '';
+    console.log(searchTerm)
+    let endPoint = ''
     this.setState({
       movies: [],
       loading: true,
       searchTerm: searchTerm,
-    });
+    })
 
     if (searchTerm === '') {
-      endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+      endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     } else {
-      endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+      endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`
     }
     // console.log(endPoint);
-    this.fetchItems(endPoint);
-  };
+    this.fetchItems(endPoint)
+  }
 
   loadMoreItems = () => {
-    let endPoint = '';
+    let endPoint = ''
     this.setState({
       loading: true,
-    });
+    })
 
     if (this.state.searchTerm === '') {
       endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${
         this.state.currentPage + 1
-      }`;
+      }`
     } else {
       endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${
         this.state.searchTerm
-      }&page=${this.state.currentPage + 1}`;
+      }&page=${this.state.currentPage + 1}`
     }
-    this.fetchItems(endPoint);
-  };
+    this.fetchItems(endPoint)
+  }
 
   fetchItems = (endPoint) => {
     fetch(endPoint)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
         this.setState({
           movies: [...this.state.movies, ...result.results],
           heroImage: result.results[7], // Purpose?  this.state.heroImage || result.results[0]
           loading: false,
           currentPage: result.page,
           totalPages: result.total_pages,
-        });
+        })
       })
-      .catch((error) => console.error('Error', error));
-  };
+      .catch((error) => console.error('Error', error))
+  }
 
   render() {
-    console.log(this.state.movies);
     return (
       <div className='rmdb-home'>
         {this.state.heroImage ? (
@@ -119,7 +117,7 @@ class Home extends Component {
                   movieId={element.id}
                   movieName={element.original_title}
                 />
-              );
+              )
             })}
           </FourColGrid>
           {this.state.loading ? <Spinner /> : null}
@@ -131,8 +129,8 @@ class Home extends Component {
           }
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
